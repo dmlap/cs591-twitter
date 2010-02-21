cb34fad1d06b2dcf628d2b0e29a5bdabcabe067a
David LaPalomento <dml@eta.(none)>
2010-02-21 13:25:46 -0500

    added httpclient dependency method to pull a single users timeline as an xml byte[]

--- src/main/java/edu/bu/PullData.java
+++ src/main/java/edu/bu/PullData.java
@@ -1,10 +1,72 @@
 package edu.bu;
 
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+
 /**
  * Pulls XML data from twitter and stores it in the filesystem.
  * 
  */
 public class PullData {
+	private static final String TWITTER_API_URL = "http://api.twitter.com";
+	private static final String TWITTER_STATUSES = TWITTER_API_URL + "/1/statuses/";
+	private static final String USER_TIMELINE_XML = TWITTER_STATUSES + "user_timeline.xml";
+	private static final String SCREEN_NAME_PARAM = "screen_name";
+	private static final String PAGE_PARAM = "page";
+	private static final String QPARAM_START = "?";
+	private static final String PARAM_ASSIGNMENT = "=";
+	private static final String PARAM_SEPARATOR = "&";
+	private static final int PAGE_COUNT = 100;
+	private final String username;
+	private final int rounds;
+	
+	public PullData(String username, int rounds) {
+		this.username = username;
+		this.rounds = rounds;
+	}
+	
+	private String apply(String base, String screenName, int page) {
+		return new StringBuilder(base)
+			.append(QPARAM_START)
+			.append(SCREEN_NAME_PARAM)
+			.append(PARAM_ASSIGNMENT)
+			.append(screenName)
+			.append(PARAM_SEPARATOR)
+			.append(PAGE_PARAM)
+			.append(PARAM_ASSIGNMENT)
+			.append(page)
+			.toString();
+	}
+	
+	public byte[] pull() throws ClientProtocolException, IOException {
+		HttpClient httpClient = new DefaultHttpClient();
+		for (int i = 0; i < PAGE_COUNT; ++i) {
+			httpClient.execute(new HttpGet(apply(USER_TIMELINE_XML,
+					username, i)), new ResponseHandler<byte[]>() {
+				@Override
+				public byte[] handleResponse(HttpResponse response)
+						throws ClientProtocolException, IOException {
+					return EntityUtils.toByteArray(response.getEntity());
+				}
+			});
+		}
+		return new byte[0];
+	}
+	
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 	}
 }
